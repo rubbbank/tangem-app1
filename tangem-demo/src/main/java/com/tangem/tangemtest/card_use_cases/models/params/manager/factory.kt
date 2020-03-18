867b27f2f0c3b97cbbf5d710c918d93f0c1df81c
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-03-18 16:35:59 +0300

    AND-262 - fixed parameterWidget

--- tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/models/params/manager/factory.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/models/params/manager/factory.kt
@@ -1,18 +1,18 @@
 package com.tangem.tangemtest.card_use_cases.models.params.manager
 
-import com.tangem.tangemtest.commons.Action
+import com.tangem.tangemtest.commons.ActionType
 import ru.dev.gbixahue.eu4d.lib.kotlin.common.BaseTypedHolder
 
 /**
  * Created by Anton Zhilenkov on 12.03.2020.
  */
-class ParamsManagerFactory : BaseTypedHolder<Action, ParamsManager>() {
+class ParamsManagerFactory : BaseTypedHolder<ActionType, ParamsManager>() {
 
     companion object {
         fun createFactory(): ParamsManagerFactory {
             return ParamsManagerFactory().apply {
-                register(Action.Scan, ScanParamsManager())
-                register(Action.Sign, SignParamsManager())
+                register(ActionType.Scan, ScanParamsManager())
+                register(ActionType.Sign, SignParamsManager())
             }
         }
     }
