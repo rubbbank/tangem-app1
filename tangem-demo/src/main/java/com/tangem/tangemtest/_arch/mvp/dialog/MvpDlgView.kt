5318b1a27394a97b470297638fd4d0b6fa500d15
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-03-19 10:57:17 +0300

    AND-262 - finished with scan and sign actions

--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/mvp/dialog/MvpDlgView.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.tangemtest._arch.mvp.dialog
-
-import com.tangem.tangemtest._arch.mvp.view.MvpView
-
-/**
- * Created by Anton Zhilenkov on 21.08.18.
- */
-interface MvpDlgView : MvpView
--- tangem-demo/src/main/java/com/tangem/tangemtest/_arch/mvp/view/MvpView.kt
+++
@@ -1,7 +0,0 @@
-package com.tangem.tangemtest._arch.mvp.view
-
-/**
- * Created by Anton Zhilenkov on 10.05.17.
- */
-
-interface MvpView
