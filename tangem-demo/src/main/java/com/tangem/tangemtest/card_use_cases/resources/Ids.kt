fa3e0c5a9d3a68aebe5fdcf614ddaf45bf68562d
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-03-25 19:10:08 +0300

    AND-262 - implemented "show/hide" for description in the entry point screen

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/card_use_cases/resources/Ids.kt
@@ -0,0 +1,22 @@
+package com.tangem.tangemtest.card_use_cases.resources
+
+import com.tangem.tangemtest._arch.structure.base.Id
+
+/**
+ * Created by Anton Zhilenkov on 25/03/2020.
+ */
+enum class ActionType: Id {
+    Scan,
+    Sign,
+    CreateWallet,
+    PurgeWallet,
+    ReadIssuerData,
+    WriteIssuerData,
+    ReadIssuerExData,
+    WriteIssuerExData,
+    ReadUserData,
+    WriteUserData,
+    Personalize,
+    Depersonalize,
+    Unknown,
+}
