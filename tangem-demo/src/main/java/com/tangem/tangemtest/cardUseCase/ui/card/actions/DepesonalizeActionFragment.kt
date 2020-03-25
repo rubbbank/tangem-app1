d1b45817dd83b2e6edb9b01ae2a156564960a684
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-03-25 22:55:01 +0300

    AND-262 - renaming by convention

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/cardUseCase/ui/card/actions/DepesonalizeActionFragment.kt
@@ -0,0 +1,16 @@
+package com.tangem.tangemtest.cardUseCase.ui.card.actions
+
+import com.tangem.tangemtest.R
+import com.tangem.tangemtest.cardUseCase.resources.ActionType
+import com.tangem.tangemtest.cardUseCase.ui.card.BaseCardActionFragment
+
+/**
+ * Created by Anton Zhilenkov on 10.03.2020.
+ */
+class DepesonalizeActionFragment : BaseCardActionFragment() {
+
+    override fun getLayoutId(): Int = R.layout.fg_depersonalize
+
+    override fun getAction(): ActionType = ActionType.Depersonalize
+
+}
