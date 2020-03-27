9d1981b8d4f8157da743cc513356af53bdfcedcb
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-03-27 15:41:10 +0300

    AND-262 - finished with the card personalization

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/Ids.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/Ids.kt
@@ -5,7 +5,7 @@ import com.tangem.tangemtest._arch.structure.Id
 /**
  * Created by Anton Zhilenkov on 26/03/2020.
  */
-enum class TlvId: Id {
+enum class TlvId : Id {
     CardId,
     TransactionOutHash,
 }
