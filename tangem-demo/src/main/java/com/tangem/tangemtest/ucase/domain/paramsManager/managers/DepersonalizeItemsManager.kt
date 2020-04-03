11eb0051ba4e3203b7c23880e50c2504022c6033
Anton Zhilenkov <gbixahue.apps@gmail.com>
2020-04-03 11:57:30 +0300

    AND-262 - fixed typo

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/DepersonalizeItemsManager.kt
@@ -4,14 +4,14 @@ import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.Id
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest._arch.structure.impl.EditTextItem
-import com.tangem.tangemtest.ucase.domain.actions.DepesonalizeAction
+import com.tangem.tangemtest.ucase.domain.actions.DepersonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
 import com.tangem.tangemtest.ucase.variants.TlvId
 
 /**
  * Created by Anton Zhilenkov on 19/03/2020.
  */
-class DepersonalizeItemsManager : BaseItemsManager(DepesonalizeAction()) {
+class DepersonalizeItemsManager : BaseItemsManager(DepersonalizeAction()) {
     override fun createItemsList(): List<Item> {
         return listOf(EditTextItem(TlvId.CardId, null))
     }
