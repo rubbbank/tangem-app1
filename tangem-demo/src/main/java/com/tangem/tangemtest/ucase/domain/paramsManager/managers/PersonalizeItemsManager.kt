08972d7874e0835a8a3851a94110dc4763b04b0c
Anton Zhilenkov <azhilenkov@tangem.com>
2020-04-08 00:07:11 +0300

    AND-297 - PersonalizeFragment depends on BaseCardActionFragment

--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/domain/paramsManager/managers/PersonalizeItemsManager.kt
@@ -4,12 +4,14 @@ import com.tangem.CardManager
 import com.tangem.tangemtest._arch.structure.abstraction.Item
 import com.tangem.tangemtest.ucase.domain.actions.PersonalizeAction
 import com.tangem.tangemtest.ucase.domain.paramsManager.ActionCallback
+import com.tangem.tangemtest.ucase.variants.personalize.converter.PersonalizeConfigConverter
+import com.tangem.tangemtest.ucase.variants.personalize.dto.PersonalizeConfig
 
 /**
  * Created by Anton Zhilenkov on 19/03/2020.
  */
 class PersonalizeItemsManager : BaseItemsManager(PersonalizeAction()) {
-    override fun createItemsList(): List<Item> = listOf()
+    override fun createItemsList(): List<Item> = PersonalizeConfigConverter().convert(PersonalizeConfig())
 
     override fun invokeMainAction(cardManager: CardManager, callback: ActionCallback) {
         action.executeMainAction(this, getAttrsForAction(cardManager), callback)
--- tangem-demo/src/main/java/com/tangem/tangemtest/ucase/tunnel/Errors.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/tunnel/Errors.kt
@@ -5,13 +5,13 @@ import com.tangem.tangemtest._arch.structure.Id
 /**
  * Created by Anton Zhilenkov on 01/04/2020.
  */
-interface Errors: Id
+interface Errors : Id
 
 enum class CardError : Errors {
     NotPersonalized,
 }
 
-enum class ItemError: Errors {
+enum class ItemError : Errors {
     BadSeries,
     BadCardNumber,
 }
