77692d845b4e16ecc878766057f1e466543e1416
Anton Zhilenkov <azhilenkov@tangem.com>
2020-04-08 23:04:48 +0300

    AND-297 - added base functionality for the ResponseScreen

---
+++ tangem-demo/src/main/java/com/tangem/tangemtest/ucase/variants/responses/Ids.kt
@@ -0,0 +1,44 @@
+package com.tangem.tangemtest.ucase.variants.responses
+
+import com.tangem.tangemtest._arch.structure.Id
+
+/**
+ * Created by Anton Zhilenkov on 08/04/2020.
+ */
+interface ResponseId : Id
+
+enum class CardId : ResponseId {
+    cardId,
+    manufacturerName,
+    status,
+    firmwareVersion,
+    cardPublicKey,
+    settingsMask,
+    cardData,
+    issuerPublicKey,
+    curve,
+    maxSignatures,
+    signingMethod,
+    pauseBeforePin2,
+    walletPublicKey,
+    walletRemainingSignatures,
+    walletSignedHashes,
+    health,
+    isActivated,
+    activationSeed,
+    paymentFlowVersion,
+    userCounter,
+    userProtectedCounter,
+}
+
+enum class CardDataId : ResponseId {
+    batchId,
+    manufactureDateTime,
+    issuerName,
+    blockchainName,
+    manufacturerSignature,
+    productMask,
+    tokenSymbol,
+    tokenContractAddress,
+    tokenDecimal,
+}
