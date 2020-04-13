cfc5c1189914cf4085ddfc4baacfaa28ed6cae93
Anton Zhilenkov <azhilenkov@tangem.com>
2020-04-13 21:58:27 +0300

    AND-301 - finished with PersonalizationPreset: default, save, load

--- tangem-demo/src/main/java/com/tangem/tangemtest/commons/Abstractions.kt
+++ tangem-demo/src/main/java/com/tangem/tangemtest/commons/Abstractions.kt
@@ -4,6 +4,13 @@ package com.tangem.tangemtest.commons
  * Created by Anton Zhilenkov on 09/04/2020.
  */
 interface Store<M> {
-    fun save(config: M)
+    fun save(value: M)
     fun restore(): M
+}
+
+interface KeyedStore<M> {
+    fun save(key: String, value: M)
+    fun restore(key: String): M
+    fun restoreAll(): Map<String, M>
+    fun delete(key: String)
 }
