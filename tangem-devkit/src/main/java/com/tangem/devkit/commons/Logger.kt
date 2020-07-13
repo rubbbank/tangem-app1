0df0acb52ce06d7f1e195431e4d9d531e95c1dfd
Roman Potapov <rpotapov@tangem.com>
2020-07-13 10:32:23 +0300

    AND-564: remove tangem-core, tangem-sdk and tangem-devkit modules

--- tangem-devkit/src/main/java/com/tangem/devkit/commons/Logger.kt
+++
@@ -1,8 +0,0 @@
-package com.tangem.devkit.commons
-
-import ru.dev.gbixahue.eu4d.lib.android.global.log.TagLogger
-
-/**
- * Created by Anton Zhilenkov on 12.03.2020.
- */
-class TangemLogger : TagLogger("TangemDemo")
