133fdb38b3da2809b9f32a7b32cfc58ba8e5785f
Anton Zhilenkov <azhilenkov@tangem.com>
2020-12-11 16:59:23 +0300

    AND-874 - segwit chips fitted by width of the chipGroup

---
+++ app/src/main/java/com/tangem/tap/common/GlobalLayoutStateHandler.kt
@@ -0,0 +1,31 @@
+package com.tangem.tap.common
+
+import android.view.View
+import android.view.ViewTreeObserver
+
+/**
+ * Created by Anton Zhilenkov on 11/12/2020.
+ */
+class GlobalLayoutStateHandler<T: View>(
+        private val view: T,
+        attachImmediately: Boolean = true
+) : ViewTreeObserver.OnGlobalLayoutListener {
+
+    var onStateChanged: ((T) -> Unit)? = null
+
+    init {
+        if (attachImmediately) attach()
+    }
+
+    fun attach() {
+        view.viewTreeObserver.addOnGlobalLayoutListener(this)
+    }
+
+    fun detach() {
+        view.viewTreeObserver.removeOnGlobalLayoutListener(this)
+    }
+
+    override fun onGlobalLayout() {
+        onStateChanged?.invoke(view)
+    }
+}
