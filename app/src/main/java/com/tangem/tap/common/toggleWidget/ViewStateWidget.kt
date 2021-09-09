9d3e06601d8a93437da1de730c20d553a5ef5ced
Anton Zhilenkov <azhilenkov@tangem.com>
2021-09-09 21:45:12 +0300

    AND-1174 fix viewStateWidget, share transitions

--- app/src/main/java/com/tangem/tap/common/toggleWidget/ViewStateWidget.kt
+++ app/src/main/java/com/tangem/tap/common/toggleWidget/ViewStateWidget.kt
@@ -1,5 +1,7 @@
 package com.tangem.tap.common.toggleWidget
 
+import android.view.View
+
 
 /**
  * Created by Anton Zhilenkov on 07/07/2020.
@@ -7,6 +9,7 @@ package com.tangem.tap.common.toggleWidget
 interface WidgetState
 
 interface ViewStateWidget {
+    val mainView: View
     fun changeState(state: WidgetState)
 }
 
