598547162dd7cde83455c27e79304080bae65132
Anton Zhilenkov <azhilenkov@tangem.com>
2021-11-11 02:32:19 +0300

    AND-1303 fixed TradeCryptoState for newly added blockchains\tokens. The ability to purchase all tokens is temporarily disabled until the Moonpay service is added marks to determine tokens

--- app/src/main/java/com/tangem/tap/domain/extensions/MoonpayStatus.kt
+++ app/src/main/java/com/tangem/tap/domain/extensions/MoonpayStatus.kt
@@ -1,19 +1,38 @@
 package com.tangem.tap.domain.extensions
 
 import com.tangem.blockchain.common.Blockchain
+import com.tangem.tap.features.wallet.redux.Currency
 import com.tangem.tap.network.moonpay.MoonpayStatus
 
 /**
  * Created by Anton Zhilenkov on 07/11/2021.
  */
-fun MoonpayStatus.buyIsAllowed(blockchain: Blockchain): Boolean {
-    if (blockchain == Blockchain.Unknown || blockchain == Blockchain.BSC) return false
+fun MoonpayStatus.buyIsAllowed(currency: Currency): Boolean {
+    if (!isBuyAllowed) return false
 
-    return isBuyAllowed && availableToBuy.contains(blockchain.currency)
+    return when (currency) {
+        is Currency.Blockchain -> {
+            if (currency.blockchain == Blockchain.Unknown || currency.blockchain == Blockchain.BSC) {
+                false
+            } else {
+                availableToBuy.contains(currency.currencySymbol)
+            }
+        }
+        is Currency.Token -> false
+    }
 }
 
-fun MoonpayStatus.sellIsAllowed(blockchain: Blockchain): Boolean {
-    if (blockchain == Blockchain.Unknown || blockchain == Blockchain.BSC) return false
+fun MoonpayStatus.sellIsAllowed(currency: Currency): Boolean {
+    if (!isSellAllowed) return false
 
-    return isSellAllowed && availableToSell.contains(blockchain.currency)
+    return when (currency) {
+        is Currency.Blockchain -> {
+            if (currency.blockchain == Blockchain.Unknown || currency.blockchain == Blockchain.BSC) {
+                false
+            } else {
+                availableToSell.contains(currency.currencySymbol)
+            }
+        }
+        is Currency.Token -> false
+    }
 }
