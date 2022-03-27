682aea12a70643592ac112fadc2d8afb9a67e3a7
Anton Zhilenkov <azhilenkov@tangem.com>
2022-03-27 22:13:46 +0300

    AND-1614 fixed merge conflicts. TapWalletManager does most work in the coroutine IO context, and the only store.dispatch() actions launches into the Main context

--- app/src/main/java/com/tangem/tap/common/extensions/Coroutine.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/Coroutine.kt
@@ -7,10 +7,6 @@ import kotlinx.coroutines.withContext
 /**
  * Created by Anton Zhilenkov on 29/09/2021.
  */
-suspend fun <T> withMainContext(block: suspend CoroutineScope.() -> T) {
-    withContext(Dispatchers.Main, block)
-}
+suspend fun <T> withMainContext(block: suspend CoroutineScope.() -> T): T = withContext(Dispatchers.Main, block)
 
-suspend fun <T> withIOContext(block: suspend CoroutineScope.() -> T) {
-    withContext(Dispatchers.IO, block)
-}
+suspend fun <T> withIOContext(block: suspend CoroutineScope.() -> T): T = withContext(Dispatchers.IO, block)
