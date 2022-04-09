99387b2cfe34d2a1238357da8fa6d86888774113
Anton Zhilenkov <azhilenkov@tangem.com>
2022-04-09 14:25:04 +0300

    AND-1592 parse colors compose <-> android.graphics

--- app/src/main/java/com/tangem/tap/common/extensions/compose/Color.kt
+++ app/src/main/java/com/tangem/tap/common/extensions/compose/Color.kt
@@ -10,7 +10,11 @@ import androidx.core.graphics.red
 /**
  * Created by Anton Zhilenkov on 17/03/2022.
  */
-fun Color.argb(): Int {
+fun Color.toAndroidGraphicsColor(): Int {
     val argb = this.toArgb()
     return android.graphics.Color.argb(argb.alpha, argb.red, argb.green, argb.blue)
+}
+
+fun Color.parse(hexColor: String): Color {
+    return Color(hexColor.removePrefix("#").toInt(16))
 }
