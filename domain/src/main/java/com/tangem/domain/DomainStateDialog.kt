42c1c4d4cbec1101a40723f287c21a320de17a9d
Anton Zhilenkov <azhilenkov@tangem.com>
2022-04-12 16:17:06 +0300

    AND-1592 fix domain module structure

---
+++ domain/src/main/java/com/tangem/domain/DomainStateDialog.kt
@@ -0,0 +1,19 @@
+package com.tangem.domain
+
+import com.tangem.common.extensions.VoidCallback
+import com.tangem.network.api.tangemTech.Coins
+
+/**
+ * Created by Anton Zhilenkov on 10/04/2022.
+ */
+interface DomainStateDialog
+
+sealed class DomainDialog : DomainStateDialog {
+
+    data class SelectTokenDialog(
+        val items: List<Coins.CheckAddressResponse.Token.Contract>,
+        val itemNameConverter: (Coins.CheckAddressResponse.Token.Contract) -> String,
+        val onSelect: (Coins.CheckAddressResponse.Token.Contract) -> Unit,
+        val onClose: VoidCallback = {}
+    ) : DomainDialog()
+}
