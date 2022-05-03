99fd7a19f8cffdd8209314a73c9fdac3e65a5610
Anton Zhilenkov <azhilenkov@tangem.com>
2022-05-03 16:39:56 +0300

    AND-1715 init domainModule from application

---
+++ domain/src/main/java/com/tangem/domain/DomainLayer.kt
@@ -0,0 +1,26 @@
+package com.tangem.domain
+
+import com.tangem.domain.features.addCustomToken.redux.AddCustomTokenAction
+import com.tangem.domain.features.addCustomToken.redux.AddCustomTokenState
+import com.tangem.domain.redux.state.ActionStateLoggerImpl
+
+/**
+ * Created by Anton Zhilenkov on 03/05/2022.
+ */
+object DomainLayer {
+    internal val actionStateLogger = ActionStateLoggerImpl()
+
+    var onInitComplete: ((DomainException?) -> Unit)? = null
+
+    fun init(data: Map<String, Any?>? = null) {
+        initActionStateLogger()
+
+        onInitComplete?.invoke(null)
+    }
+
+    private fun initActionStateLogger() {
+        val factory = actionStateLogger.actionStateConvertersFactory
+
+        factory.addConverter(AddCustomTokenAction::class.java, AddCustomTokenState.Converter())
+    }
+}
