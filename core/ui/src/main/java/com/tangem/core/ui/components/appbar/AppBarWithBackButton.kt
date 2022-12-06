tangem/tangem-app-android

AND-2463 Add OnBackClicked and error alert (#383)
Co-authored-by: vladislav kozarez <Kozarez.V.Se@sberbank.ru>

core/ui/src/main/java/com/tangem/core/ui/components/appbar/AppBarWithBackButton.kt
@@ -1,5 +1,6 @@
 package com.tangem.core.ui.components.appbar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -18,22 +19,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.R
 import com.tangem.core.ui.res.TangemTheme
 
-@Preview(widthDp = 360, heightDp = 56, showBackground = true)
-@Composable
-fun PreviewAppBarWithBackButtonInLightTheme() {
-    TangemTheme(isDarkTheme = false) {
-        AppBarWithBackButton(text = "Title", onBackClick = {})
-    }
-}
-
-@Preview(widthDp = 360, heightDp = 56, showBackground = true)
-@Composable
-fun PreviewAppBarWithBackButtonInDarkTheme() {
-    TangemTheme(isDarkTheme = true) {
-        AppBarWithBackButton(text = "Title", onBackClick = {})
-    }
-}
-
 /**
  * App bar with back button and optional title
  *
@@ -47,8 +32,9 @@ fun PreviewAppBarWithBackButtonInDarkTheme() {
 fun AppBarWithBackButton(text: String? = null, onBackClick: () -> Unit) {
     Row(
         modifier = Modifier
-            .padding(all = dimensionResource(R.dimen.spacing16))
-            .fillMaxWidth(),
+            .background(MaterialTheme.colors.primary)
+            .fillMaxWidth()
+            .padding(all = dimensionResource(R.dimen.spacing16)),
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.spacing16)),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -68,5 +54,21 @@ fun AppBarWithBackButton(text: String? = null, onBackClick: () -> Unit) {
                 style = MaterialTheme.typography.subtitle1,
             )
         }
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 56, showBackground = true)
+@Composable
+fun PreviewAppBarWithBackButtonInLightTheme() {
+    TangemTheme(isDarkTheme = false) {
+        AppBarWithBackButton(text = "Title", onBackClick = {})
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 56, showBackground = true)
+@Composable
+fun PreviewAppBarWithBackButtonInDarkTheme() {
+    TangemTheme(isDarkTheme = true) {
+        AppBarWithBackButton(text = "Title", onBackClick = {})
     }
 }

features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AgreementText.kt
@@ -22,28 +22,8 @@ import com.tangem.core.ui.res.TextColorType
 import com.tangem.core.ui.res.textColor
 import com.tangem.feature.referral.presentation.R
 
-@Preview(widthDp = 360, showBackground = true)
 @Composable
-fun Preview_AgreementText_InLightTheme() {
-    TangemTheme(isDarkTheme = false) {
-        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
-            AgreementText(firstPartResId = R.string.referral_tou_not_enroled_prefix, onClicked = {})
-        }
-    }
-}
-
-@Preview(widthDp = 360, showBackground = true)
-@Composable
-fun Preview_AgreementText_InDarkTheme() {
-    TangemTheme(isDarkTheme = true) {
-        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
-            AgreementText(firstPartResId = R.string.referral_tou_not_enroled_prefix, onClicked = {})
-        }
-    }
-}
-
-@Composable
-fun AgreementText(@StringRes firstPartResId: Int, onClicked: () -> Unit) {
+internal fun AgreementText(@StringRes firstPartResId: Int, onClicked: () -> Unit) {
     val agreementText = annotatedAgreementString(firstPart = stringResource(firstPartResId))
     ClickableText(
         text = agreementText,
@@ -72,6 +52,26 @@ private fun annotatedAgreementString(firstPart: String): AnnotatedString {
         }
         withStyle(SpanStyle(color = MaterialTheme.colors.textColor(type = TextColorType.TERTIARY))) {
             append(" ${stringResource(id = R.string.referral_tou_suffix)}")
+        }
+    }
+}
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_AgreementText_InLightTheme() {
+    TangemTheme(isDarkTheme = false) {
+        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
+            AgreementText(firstPartResId = R.string.referral_tou_not_enroled_prefix, onClicked = {})
+        }
+    }
+}
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_AgreementText_InDarkTheme() {
+    TangemTheme(isDarkTheme = true) {
+        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
+            AgreementText(firstPartResId = R.string.referral_tou_not_enroled_prefix, onClicked = {})
         }
     }
 }

rubbbank/tangem-app1

core/ui/src/main/java/com/tangem/core/ui/components/appbar/AppBarWithBackButton.kt
@@ -1,5 +1,6 @@
 package com.tangem.core.ui.components.appbar
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
@@ -18,22 +19,6 @@ import androidx.compose.ui.tooling.preview.Preview
 import com.tangem.core.ui.R
 import com.tangem.core.ui.res.TangemTheme
 
-@Preview(widthDp = 360, heightDp = 56, showBackground = true)
-@Composable
-fun PreviewAppBarWithBackButtonInLightTheme() {
-    TangemTheme(isDark = false) {
-        AppBarWithBackButton(text = "Title", onBackClick = {})
-    }
-}
-
-@Preview(widthDp = 360, heightDp = 56, showBackground = true)
-@Composable
-fun PreviewAppBarWithBackButtonInDarkTheme() {
-    TangemTheme(isDark = true) {
-        AppBarWithBackButton(text = "Title", onBackClick = {})
-    }
-}
-
 /**
  * App bar with back button and optional title
  *
@@ -47,8 +32,9 @@ fun PreviewAppBarWithBackButtonInDarkTheme() {
 fun AppBarWithBackButton(text: String? = null, onBackClick: () -> Unit) {
     Row(
         modifier = Modifier
-            .padding(all = dimensionResource(R.dimen.spacing16))
-            .fillMaxWidth(),
+            .background(MaterialTheme.colors.primary)
+            .fillMaxWidth()
+            .padding(all = dimensionResource(R.dimen.spacing16)),
         horizontalArrangement = Arrangement.spacedBy(dimensionResource(R.dimen.spacing16)),
         verticalAlignment = Alignment.CenterVertically,
     ) {
@@ -68,5 +54,21 @@ fun AppBarWithBackButton(text: String? = null, onBackClick: () -> Unit) {
                 style = MaterialTheme.typography.subtitle1,
             )
         }
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 56, showBackground = true)
+@Composable
+fun PreviewAppBarWithBackButtonInLightTheme() {
+    TangemTheme(isDark = false) {
+        AppBarWithBackButton(text = "Title", onBackClick = {})
+    }
+}
+
+@Preview(widthDp = 360, heightDp = 56, showBackground = true)
+@Composable
+fun PreviewAppBarWithBackButtonInDarkTheme() {
+    TangemTheme(isDark = true) {
+        AppBarWithBackButton(text = "Title", onBackClick = {})
     }
 }

features/referral/presentation/src/main/java/com/tangem/feature/referral/ui/AgreementText.kt
@@ -22,28 +22,8 @@ import com.tangem.core.ui.res.TextColorType
 import com.tangem.core.ui.res.textColor
 import com.tangem.feature.referral.presentation.R
 
-@Preview(widthDp = 360, showBackground = true)
 @Composable
-fun Preview_AgreementText_InLightTheme() {
-    TangemTheme(isDark = false) {
-        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
-            AgreementText(firstPartResId = R.string.referral_tos_not_enroled_prefix, onClicked = {})
-        }
-    }
-}
-
-@Preview(widthDp = 360, showBackground = true)
-@Composable
-fun Preview_AgreementText_InDarkTheme() {
-    TangemTheme(isDark = true) {
-        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
-            AgreementText(firstPartResId = R.string.referral_tos_not_enroled_prefix, onClicked = {})
-        }
-    }
-}
-
-@Composable
-fun AgreementText(@StringRes firstPartResId: Int, onClicked: () -> Unit) {
+internal fun AgreementText(@StringRes firstPartResId: Int, onClicked: () -> Unit) {
     val agreementText = annotatedAgreementString(firstPart = stringResource(firstPartResId))
     ClickableText(
         text = agreementText,
@@ -72,6 +52,26 @@ private fun annotatedAgreementString(firstPart: String): AnnotatedString {
         }
         withStyle(SpanStyle(color = MaterialTheme.colors.textColor(type = TextColorType.TERTIARY))) {
             append(" ${stringResource(id = R.string.referral_tos_suffix)}")
+        }
+    }
+}
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_AgreementText_InLightTheme() {
+    TangemTheme(isDark = false) {
+        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
+            AgreementText(firstPartResId = R.string.referral_tos_not_enroled_prefix, onClicked = {})
+        }
+    }
+}
+
+@Preview(widthDp = 360, showBackground = true)
+@Composable
+fun Preview_AgreementText_InDarkTheme() {
+    TangemTheme(isDark = true) {
+        Box(modifier = Modifier.background(MaterialTheme.colors.primary)) {
+            AgreementText(firstPartResId = R.string.referral_tos_not_enroled_prefix, onClicked = {})
         }
     }
 }