8d937ef93c873b7f6939179787307cb0cea39f68
Anton Zhilenkov <azhilenkov@tangem.com>
2023-01-23 17:01:14 +0300

    AND-2836/AND-2839 added SignIn events

---
+++ app/src/main/java/com/tangem/tap/common/analytics/events/SignIn.kt
@@ -0,0 +1,17 @@
+package com.tangem.tap.common.analytics.events
+
+import com.tangem.core.analytics.AnalyticsEvent
+
+/**
+ * Created by Anton Zhilenkov on 23.01.2023.
+ */
+sealed class SignIn(
+    event: String,
+    params: Map<String, String> = mapOf(),
+    error: Throwable? = null,
+) : AnalyticsEvent("Sign In", event, params, error) {
+
+    class ScreenOpened : SignIn(event = "Sing In Screen Opened")
+    class ButtonBiometricSignIn : SignIn(event = "Button - Biometric Sign In")
+    class ButtonCardSignIn : SignIn(event = "Button - Card Sign In")
+}
